bc62e597cd4160d7f061a8c1d23e5491c2490e39
elmir mammadli <elmir.memmedov6071@gmail.com>
2024-09-20 10:57:02 +0000

    AppJs

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 "use strict";
 $(document).ready(function () {
-    
-   
+
+
+
+
+    $("#me").click(function (e) {
+        e.preventDefault();
+        function myel(myclass = "") {
+            if (!$.trim($("#inputme").val()) == "") {
+                let vall = $("inputme").val()
+                let newH3 = $('<h3> </h3>')
+                let newDiv = $('<div class="line"> </div>')
+                let bigDiv = $(`<div ${myclass}> </div>`)
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiv)
+                return bigDiv
+            }
+            else {
+                return
+            }
+        }
+
+        $("#messageyou").append(myel())
+        $("#messageme").append(myel('class="end"'))
+        $("#inputme").val("")
+
+    })
+
 
 
 
